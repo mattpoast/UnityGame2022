f7c43e6b9efa0b6405b0ad2b710ea79afd1edc14
matt poast <matt.poast@gmail.com>
2022-09-04 12:08:29 -0600

    Coroutines

---
+++ ColorMatchingGame/ColorMatchinGame/Assets/Scripts/CoroutineBehavior.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+public class CoroutineBehavior : MonoBehaviour
+{
+   public UnityEvent startEvent, startCountEvent, repeatCountEvent, endCountEvent, repeatUntilFalseEvent;
+
+   public bool canRun;
+   public IntData counterNum;
+   public float seconds = 3.0f;
+   private WaitForSeconds wfsObj;
+   private WaitForFixedUpdate wffuObj;
+
+   private void Start()
+   {
+      startEvent.Invoke();
+   }
+
+   public void StartCounting()
+   {
+      StartCoroutine(Counting());
+   }
+   IEnumerator Counting()
+   {
+      wfsObj = new WaitForSeconds(seconds);
+      wffuObj = new WaitForFixedUpdate();
+      startCountEvent.Invoke();
+      yield return wfsObj;
+      while (counterNum.value > 0)
+      {
+         repeatCountEvent.Invoke();
+         counterNum.value--;
+         yield return wfsObj;
+      }
+      endCountEvent.Invoke();
+   }
+
+   public void StartRepeatUntilFalse()
+   {
+      canRun = true;
+      StartCoroutine(RepeatUntilFalse());
+   }
+   private IEnumerator RepeatUntilFalse()
+   {
+      while (canRun)
+      {
+         yield return wfsObj;
+         repeatUntilFalseEvent.Invoke();
+      }
+   }
+}
